7739394f0092432922ac43d1ac9622b5a0c37fd4
luhaolu <therealhaolu@gmail.com>
2017-11-24 16:41:11 -0800

    add the missing files

---
+++ EventPatternMatching/FailureDetector.cs
@@ -0,0 +1,84 @@
+﻿
+using EventPatternMatching;
+using RC.CodingChallenge;
+using System;
+using System.Collections.Concurrent;
+using System.IO;
+
+namespace EventPatternMatching
+{
+    public class FailureDetector : IEventCounter
+    {
+        private static FailureDetector instance;
+
+        private ConcurrentDictionary<string, int> eventHistory = new ConcurrentDictionary<string, int>();
+
+        private FailureDetector() { }
+
+        public static FailureDetector Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new FailureDetector();
+                }
+                return instance;
+            }
+        }
+
+        public int GetEventCount(string deviceID)
+        {
+            int eventCount = 0;
+            this.eventHistory.TryGetValue(deviceID, out eventCount);
+            return eventCount;
+        }
+
+        public void ParseEvents(string deviceID, StreamReader eventLog)
+        {
+            LineEntry previous = new LineEntry();
+            LineEntry current;
+            Boolean sequenceMatch = false;
+
+            while (!eventLog.EndOfStream)
+            {
+                // read a line from log
+                String line = eventLog.ReadLine();
+                string[] parsedLine = line.Split('\t');
+
+                try
+                {
+                    current = new LineEntry(Convert.ToDateTime(parsedLine[0]), Int32.Parse(parsedLine[1]));
+                }
+                catch (Exception)
+                {
+                    // unable to convert from string, skip line
+                    continue;
+                }
+
+                // if no stage changed, skip the line
+                if (current.Stage == previous.Stage) { continue; }
+
+                // if stage changed from 3 to 2, check for stage 3's duration
+                else if (previous.Stage == 3 && current.Stage == 2)
+                {
+                    // check duration
+                    TimeSpan diff = current.Timestamp - previous.Timestamp;
+                    if (diff.TotalMinutes >= 5.0)
+                    {
+                        sequenceMatch = true;
+                    }
+                }
+
+                else if (sequenceMatch && current.Stage == 0)
+                {
+                    // Failere event detected, update history and reset status
+                    this.eventHistory.AddOrUpdate(deviceID, 1, (key, oldValue) => oldValue + 1);
+                    sequenceMatch = false;
+                }
+
+                previous = current;
+            }
+        }
+    }
+}
---
+++ EventPatternMatching/LineEntry.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventPatternMatching
+{
+    class LineEntry
+    {
+        private DateTime timestamp;
+        private Int32 stage;
+
+        public LineEntry()
+        {
+
+        }
+
+        public LineEntry(DateTime timestamp, Int32 stage)
+        {
+            this.timestamp = timestamp;
+            this.stage = stage;
+        }
+
+        public DateTime Timestamp
+        {
+            get
+            {
+                return this.timestamp;
+            }
+        }
+        public Int32 Stage
+        {
+            get
+            {
+                return this.stage;
+            }
+        }
+
+    }
+}
